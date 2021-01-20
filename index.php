1d9576f89e2cc5728781fe117b47c07a7fb1c091
Tiago leandro <tiago.leandro910@gmail.com>
2021-01-20 11:07:55 -0800

    inicio

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>OLX-Black</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css"/> 
+    <link rel="stylesheet" hred="assets/css/style.css"/>
+    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+<body>
+
+
+</body>
+
+
+
+</html>
